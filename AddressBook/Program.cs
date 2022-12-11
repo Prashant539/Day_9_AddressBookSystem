55ef149a4a72bdf675098a0372441dddeba8f5ea
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-11 15:38:25 +0530

    Create a contacts

---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AddressBook
+{
+    internal class Program
+    {
+        public static List<Contacts> Person = new List<Contacts>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook.Person.CreateContacts();
+        }
+    }
+}
